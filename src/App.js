d6abd9d44213f7de97e92b7dc39cddf4d1629f32
Sergei Kompanietc <delightvlg@gmail.com>
2021-06-07 18:15:19 +0300

    feat: spinner added

--- src/App.js
+++ src/App.js
@@ -11,18 +11,22 @@ import './App.css';
 
 function App() {
   const [isLoginError, setIsLoginError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const history = useHistory();
 
   const submitHandler = (formData) => {
+    setIsLoading(true);
     mainApi.getToken(formData.username, formData.password)
       .then((data) => {
         if (data) {
+          setIsLoading(false);
           localStorage.setItem('token', data.token);
           localStorage.setItem('currentUser', formData.username);
           history.push('/user-page');
         }
       })
       .catch((err) => {
+        setIsLoading(false);
         setIsLoginError(true);
         console.error(err);
       });
@@ -37,7 +41,11 @@ function App() {
         </Route>
 
         <Route path="/login">
-          <LoginPage onFormSubmit={submitHandler} isLoginError={isLoginError} />
+          <LoginPage
+            onFormSubmit={submitHandler}
+            isLoginError={isLoginError}
+            isLoading={isLoading}
+          />
         </Route>
 
         <Route path="/user-page">
